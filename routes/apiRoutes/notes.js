bd7909124c40ac24c9cccee5032b11d2cef258ed
Stephon Smith <smithwrestling89@gmail.com>
2020-08-15 13:05:59 -0400

    Added ability to create new notes in JSON file
    Added helper function to be able to add new notes to the JSON file using the post request.

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,33 +1,37 @@
 const router = require('express').Router();
 const notes = require('../../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+const createNewNote = require('../../lib/notes');
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     // When user requests the notes
     // Send the notes in a json format
     res.json(notes);
 })
-
-router.get('/api/notes/:id', (req, res) => {
+router.get('/notes/:id', (req, res) => {
     // find the note by id
     // if there is a matching result then return it
     // otherwise sending 404 error
 })
 
-router.get('/api/notes/:title', (req, res) => {
-    // find note by keyword
-    // if it matches then return it
-    // otherwise return 404 error
+router.get('/notes/:title', (req, res) => {
+    const results = 
+    res.json(notes)
 })
 
-router.post('/api/notes', (req, res) => {
-    // set id for the note
+router.post('/notes', (req, res) => {
+    // set unique id for the note
+    req.body.id = uuidv4();
+    const newNote = createNewNote(req.body, notes);
+    // res.json(newNote);
+    res.json(newNote);
     // validate the note
         // if note doesn't contain title
         // if note doesn't contain body text
         // keywords optional? mandatory?
 })
 
-router.get('/api/*', (req,res) => {
+router.get('/*', (req,res) => {
     res.status(404).send('This is not a valid request.');
 })
 
